c50ccc9cbb99ed8c9d519e5f32442651a5f7b481
root <sinisa.lugonja1@gmail.com>
2019-04-04 13:52:21 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>Welcome to Your Vue.js App</h1>
+    <note></note>
+  </div>
+</template>
+
+<script>
+import note from './components/note'
+
+export default {
+  name: 'notes',
+  components:{
+    note
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/note.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <input type="text" placeholder="title" v-model="newNote" @keyup.enter="addNote">
+        <input type="text" placeholder="message" v-model="newNote" @keyup.enter="addNote">
+        <div v-for="(note, index) in notes" :key="note.id">
+            <h4>{{note.title}}</h4>
+            <p>{{note.message}}</p>
+
+        <div class="remove-note" @click="removeNote(index)">
+            &times;
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'note',
+    data() {
+        return {
+            newNote:'',
+            idForNote:3,
+            notes:[
+                {
+                    'id':1,
+                    'title': "groceries",
+                    'message': "do that",
+                },
+                {
+                    'id':1,
+                    'title': "shit pants",
+                    'message': "dont that",
+                }
+            ]
+        }
+    },
+    methods:{
+        addNote() {
+            if(this.newNote.trim().length==0){
+                return
+            }
+
+            this.notes.push({
+                id: this.idForNote,
+                title: this.newNote,
+            })
+
+            this.newNote = ''
+            this.idForNote++
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
