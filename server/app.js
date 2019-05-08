9c1f9d484eea6888dcdc45bed3ff5a08cd9fb668
Sinisa Lugonja <47387438+lsinisa@users.noreply.github.com>
2019-05-08 13:19:25 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ const enableCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', '*');
     res.header('Access-Control-Allow-Headers', '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 }
 app.use(enableCrossDomain)
